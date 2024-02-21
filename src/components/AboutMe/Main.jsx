595c97418bdb214cd2fb11ceb4b37bd4543eb165
Ryan Luis <RyanCLuis@gmail.com>
2024-02-21 11:07:02 -0800

    styles about me

--- src/components/AboutMe/Main.jsx
+++ src/components/AboutMe/Main.jsx
@@ -11,8 +11,8 @@ const Main = () => {
             <div className='mainContainer'>
                 <div className='innerContainer'>
                     <div className='leftContainer'>
-                        <h2>Hi, I am <br /> Ryan Luis</h2>
-                        <p>As a Software Engineer with a foundation in graphic design and AutoCAD, I embody a versatile blend of creativity, professionalism, and unwavering dedication. My acute attention to detail enables me to execute tasks with precision, ensuring excellence in everything I do. Thriving on collaboration, I eagerly seek opportunities to contribute to impactful projects, driven by the aspiration to make a meaningful difference in the world.</p>
+                        <h2>Hi, I'm Ryan Luis</h2>
+                        <p>As a Software Engineer with a foundation in graphic design and AutoCAD, I embody a versatile blend of creativity, professionalism, and unwavering dedication. <br />My acute attention to detail enables me to execute tasks with precision, ensuring excellence in everything I do. <br />Thriving on collaboration, I eagerly seek opportunities to contribute to impactful projects, driven by the aspiration to make a meaningful difference in the world.</p>
                         <button>Resume</button>
                     </div>
 
