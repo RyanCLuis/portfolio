31111dd09583ba34b9a42b4c8b3d4408c57a16e7
Ryan Luis <RyanCLuis@gmail.com>
2024-02-29 10:51:04 -0800

    Adds Skills

---
+++ src/components/data/skills.js
@@ -0,0 +1,101 @@
+export const skills = [
+    {
+        title: 'Frontend',
+        skills: [
+            {
+                name: 'React',
+                image: '/images/Skills/react.png'
+            },
+            {
+                name: 'JavaScript',
+                image: '/images/Skills/javascript.png'
+            },
+            {
+                name: 'HTML',
+                image: '/images/Skills/html.png'
+            },
+            {
+                name: 'CSS',
+                image: '/images/Skills/css.png'
+            },
+            {
+                name: 'Bootstrap',
+                image: '/images/Skills/bootstrap.png'
+            },
+            {
+                name: 'Material-UI',
+                image: '/images/Skills/material-ui.png'
+            },
+        ]
+    },
+    {
+        title: 'Backend',
+        skills: [
+            {
+                name: 'Node.js',
+                image: '/images/Skills/nodejs.png'
+            },
+            {
+                name: 'Express',
+                image: '/images/Skills/express.png'
+            },
+            {
+                name: 'MongoDB',
+                image: '/images/Skills/mongodb.png'
+            },
+            {
+                name: 'PostgreSQL',
+                image: '/images/Skills/postgresql.png'
+            },
+            {
+                name: 'Django',
+                image: '/images/Skills/django.png'
+            },
+            {
+                name: 'Python',
+                image: '/images/Skills/python.png'
+            },
+        ]
+    },
+    {
+        title: 'Other',
+        skills: [
+            {
+                name: 'Git',
+                image: '/images/Skills/git.png'
+            },
+            {
+                name: 'GitHub',
+                image: '/images/Skills/github.png'
+            },
+            {
+                name: 'Postman',
+                image: '/images/Skills/postman.png'
+            },
+            {
+                name: 'Heroku',
+                image: '/images/Skills/heroku.png'
+            },
+            {
+                name: 'Netlify',
+                image: '/images/Skills/netlify.png'
+            },
+            {
+                name: 'VS Code',
+                image: '/images/Skills/vscode.png'
+            },
+            {
+                name: 'Adobe Photoshop',
+                image: '/images/Skills/adobephotoshop.png'
+            },
+            {
+                name: 'AutoCAD',
+                image: '/images/Skills/autocad.png'
+            },
+            {
+                name: 'Sony Vegas',
+                image: '/images/Skills/sonyvegas.png'
+            }
+        ]
+    }
+]
