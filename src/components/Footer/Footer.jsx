b42b45f70febc0aa3e73ae149519d9a127d4ff3a
Ryan Luis <RyanCLuis@gmail.com>
2024-03-01 09:44:40 -0800

    Makes Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,50 @@
+import React from 'react'
+import './Footer.css'
+
 const Footer = () => {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className="FooterContainer">
+            <div className="FooterWrapper">
+                <h1 className="Logo">Ryan Luis</h1>
+                <ul className="FooterNav">
+                    <a className="FooterNavLink" href="#About">About</a>
+                    <a className="FooterNavLink" href="#Skills">Skills</a>
+                    <a className="FooterNavLink" href="#Projects">Projects</a>
+                    <a className="FooterNavLink" href="#Contact">Contact</a>
+                </ul>
+                <div className="SocialMediaIcons">
+                    <a
+                        href="https://www.instagram.com/ry_guygaming/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            className='SocialMediaIcon'
+                            src="/images/instagram.png" alt="insta" />
+                    </a>
+                    <a
+                        href="https://twitter.com/Ry_GuyGaming"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            className='SocialMediaIcon' 
+                            src="/images/Twitter.png" alt="twitter" />
+                    </a>
+                    <a 
+                        href="https://www.linkedin.com/in/ryancluis/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            className='SocialMediaIcon'
+                            src="/images/linkedin.png" alt="linkedin" />
+                    </a>
+                </div>
+                <footer className='Copyright'>
+                    <p>© 2024 Ryan Luis</p>
+                </footer>
+            </div>
         </footer>
     )
 }
